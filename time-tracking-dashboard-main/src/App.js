2b42fb2b81bec811fe9e82280aa71e59a0aefd5e
Ahmed Ikram <ikramahmed398@gmail.com>
2021-10-31 17:47:17 +0400

    Setup react app for time tracking challenge

---
+++ time-tracking-dashboard-main/src/App.js
@@ -0,0 +1,13 @@
+import './css/style.css'
+import Attribution from './components/Attribution';
+
+const App = () => {
+  return (
+    <div>
+      <div>Hello react</div>
+      <Attribution />
+    </div>
+  )
+}
+
+export default App;
---
+++ time-tracking-dashboard-main/src/components/Attribution.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Attribution = () => {
+    return(
+        <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="https://github.com/ahmed-cmyk" rel="noreferrer" target="_blank">Ahmed Ikram</a>.
+        </div>
+    )
+}
+
+export default Attribution
